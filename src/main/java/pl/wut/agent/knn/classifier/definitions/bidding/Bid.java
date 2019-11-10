cf69b67e3ba5ee1452ea68439fa7830eb6c92cdf
lukasz.grebowski <lukasz.grebowski@asseco.pl>
2019-11-10 14:54:54 +0100

    Add object UUID to bid structure

--- src/main/java/pl/wut/agent/knn/classifier/definitions/bidding/Bid.java
+++ src/main/java/pl/wut/agent/knn/classifier/definitions/bidding/Bid.java
@@ -4,9 +4,11 @@ import jade.core.AID;
 import lombok.RequiredArgsConstructor;
 
 import java.io.Serializable;
+import java.util.UUID;
 
 @RequiredArgsConstructor(staticName = "from")
 public class Bid implements Serializable {
     private final AID bidder;
     private final double value;
+    private final UUID objectUuid;
 }

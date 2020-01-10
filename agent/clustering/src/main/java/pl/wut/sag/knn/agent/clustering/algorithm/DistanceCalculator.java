c0870305e4e0ecfbf6e4f685191c3c10c6ff0a76
lukasz.grebowski <lukasz.grebowski@asseco.pl>
2020-01-10 15:08:28 +0100

    Add methods for computation of most distant element

--- agent/clustering/src/main/java/pl/wut/sag/knn/agent/clustering/algorithm/DistanceCalculator.java
+++ agent/clustering/src/main/java/pl/wut/sag/knn/agent/clustering/algorithm/DistanceCalculator.java
@@ -4,15 +4,23 @@ package pl.wut.sag.knn.agent.clustering.algorithm;
 import pl.wut.sag.knn.infrastructure.collection.ImmutableList;
 import pl.wut.sag.knn.ontology.object.ObjectWithAttributes;
 
+import java.util.Comparator;
+
 public interface DistanceCalculator {
     double calculateDistance(final ObjectWithAttributes one, final ObjectWithAttributes other);
 
-    default double calculateAverageDistaneInCluster(ImmutableList<ObjectWithAttributes> elements) {
+    default double calculateAverageDistance(final ImmutableList<ObjectWithAttributes> elements, final ObjectWithAttributes element) {
         return elements.stream()
-                .mapToDouble(e -> elements.stream()
-                        .filter(el -> !(el == e))
-                        .mapToDouble(el -> calculateDistance(e, el)).average().getAsDouble()
-                ).average().getAsDouble();
+                .filter(el -> !(el == element))
+                .mapToDouble(el -> calculateDistance(element, el)).average().getAsDouble();
+    }
 
+    default double calculateAverageDistaneInCluster(final ImmutableList<ObjectWithAttributes> elements) {
+        return elements.stream()
+                .mapToDouble(e -> calculateAverageDistance(elements, e)).average().getAsDouble();
+    }
+
+    default ObjectWithAttributes findMostDistantElement(final ImmutableList<ObjectWithAttributes> elements) {
+        return elements.stream().max(Comparator.comparingDouble(e -> calculateAverageDistance(elements, e))).get();
     }
 }
